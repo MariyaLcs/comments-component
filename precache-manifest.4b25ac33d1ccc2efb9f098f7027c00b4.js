e0fda6e6852f8dd7e577afd9a153836d127bd19c
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-01 17:36:26 +0100

    Updates

---
+++ precache-manifest.4b25ac33d1ccc2efb9f098f7027c00b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbb86fad9343046c8ba76162f7f1395",
+    "url": "/comments-component/index.html"
+  },
+  {
+    "revision": "3a433b9656e70f2900a1",
+    "url": "/comments-component/static/js/2.83948464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comments-component/static/js/2.83948464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5c3eeb2f0a212e2749",
+    "url": "/comments-component/static/js/main.9395fa11.chunk.js"
+  },
+  {
+    "revision": "021cc7b58360258626b8",
+    "url": "/comments-component/static/js/runtime-main.75f21178.js"
+  }
+]);
